96986c186b945f9d8eeb343b9fcc31e8f1057891
Anasteisa97 <nas.lazarewa@yandex.ru>
2022-10-03 18:02:33 +0300

    Добавила вызов функций

--- js/main.js
+++ js/main.js
@@ -5,6 +5,8 @@ const getRandomNumber = (min, max) => {
   const result = Math.floor(Math.random() * (max - min + 1)) + min;
   return ((min >= max) || min < 0 || !Number.isInteger(result)) ? NaN : result;
 };
+getRandomNumber(6, 666);
 
 //Функция для проверки максимальной длины строки.
 const checkMaxLengthOfStr = (string, maxLength) => string.length <= maxLength;
+checkMaxLengthOfStr('dfvnv rfgrieognrk grjegnr', 22);
