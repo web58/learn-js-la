555e986ac5d9271fc926997e742c9540e8d7f882
Anasteisa97 <nas.lazarewa@yandex.ru>
2022-10-19 09:55:25 +0300

    Добавлена передача url изображения в .picture__img

--- js/users-pictures.js
+++ js/users-pictures.js
@@ -12,6 +12,7 @@ const picturesListFragment = document.createDocumentFragment();
 usersPictures.forEach(({url, likes, comments}) => {
   const pictureElement = pictureTemplate.cloneNode(true);
   pictureElement.href = url;
+  pictureElement.querySelector('.picture__img').src = url;
   pictureElement.querySelector('.picture__likes').textContent = likes;
   pictureElement.querySelector('.picture__comments').textContent = comments;
   picturesListFragment.appendChild(pictureElement);
